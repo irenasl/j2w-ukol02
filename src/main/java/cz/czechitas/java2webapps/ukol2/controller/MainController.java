801962c6f14afc0f37f6410236d1034bf01cb742
Irena Slahorkova <irena.slahorkova@wandera.com>
2021-04-11 23:40:07 +0200

    Ukol 02, nedelni varianta

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -47,6 +47,7 @@ public class MainController {
 
         List<String> seznamKoduProObrazky = Arrays.asList("i6Wc5qZO5MQ","N0wr6vRpwCQ","jNYFmC5ZjoI","RFHFV7lVQBY","tzFVsg6nWtM","bAcR26qMPnY","71vAb1FXB6g","Q1p7bh3SHj8","k2zWqv_yfNM","ejEdpGYGgrA");
         pozadi = "background-image: url(https://source.unsplash.com/" + seznamKoduProObrazky.get(random.nextInt(10)) + "/1600x900";
+
         result.addObject("pozadi", pozadi);
 
         return result;
