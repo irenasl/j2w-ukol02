2a213adb7cbfb66daa664c096bea1dd7495bc5b4
Irena Slahorkova <irena.slahorkova@wandera.com>
2021-04-25 09:12:14 +0200

    finalni varianta

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -17,6 +16,28 @@ import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
+    private final List<String> seznamKoduProObrazky;
+    private final List<String> seznamCitatu;
+    private final Random random;
+
+    public MainController() throws IOException {
+        seznamKoduProObrazky = Arrays.asList("i6Wc5qZO5MQ","N0wr6vRpwCQ","jNYFmC5ZjoI","RFHFV7lVQBY","tzFVsg6nWtM",
+                               "bAcR26qMPnY","71vAb1FXB6g","Q1p7bh3SHj8","k2zWqv_yfNM","ejEdpGYGgrA");
+        seznamCitatu = readAllLines("citaty.txt");
+        random = new Random();
+    }
+
+    @GetMapping("/")
+    public ModelAndView citaty() {
+
+        ModelAndView result = new ModelAndView("citaty");
+
+        String pozadi =  seznamKoduProObrazky.get(random.nextInt(seznamKoduProObrazky.size()));
+        result.addObject("pozadi", String.format("https://source.unsplash.com/%s/1600x900", pozadi));
+        result.addObject("citat", seznamCitatu.get(random.nextInt(seznamCitatu.size())));
+
+        return result;
+    }
 
     private static List<String> readAllLines(String resource) throws IOException {
         //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
@@ -32,23 +53,5 @@ public class MainController {
                     .lines()
                     .collect(Collectors.toList());
         }
-    }
-
-    private final Random random = new Random();
-    List<String> seznamKoduProObrazky = Arrays.asList("i6Wc5qZO5MQ","N0wr6vRpwCQ","jNYFmC5ZjoI","RFHFV7lVQBY","tzFVsg6nWtM","bAcR26qMPnY","71vAb1FXB6g","Q1p7bh3SHj8","k2zWqv_yfNM","ejEdpGYGgrA");
-    List<String> seznamTextu = readAllLines("citaty.txt");
-
-    @GetMapping("/")
-    public ModelAndView citaty() throws IOException {
-
-        String pozadi;
-        ModelAndView result = new ModelAndView("citaty");
-
-        result.addObject("citat", seznamTextu.get(random.nextInt(8)));
-
-        pozadi = "background-image: url(https://source.unsplash.com/" + seznamKoduProObrazky.get(random.nextInt(10)) + "/1600x900";
-        result.addObject("pozadi", pozadi);
-
-        return result;
     }
 }
